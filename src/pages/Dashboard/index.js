2a42e049539387f000626b390c2918cc8541f706
Delfio <delfio_eu@hotmail.com>
2019-11-13 00:31:33 -0400

    Listagem dos agendamentos com a função de deletar

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -7,8 +7,6 @@ import { Container, Title, List } from './styles';
 import Background from '../../components/Background';
 import Appointment from '../../components/Appointment';
 
-const data = [1, 2, 3, 4, 5];
-
 export default function Dashboard() {
   const [appointments, setAppointments] = useState([]);
 
@@ -21,6 +19,20 @@ export default function Dashboard() {
     loadAppointments();
   }, [])
 
+  async function handleCancel(id){
+      const response = await api.delete(`appointments/${id}`)
+
+      setAppointments(
+        appointments.map( appointment =>
+          appointment.id === id ? {
+            ...appointment,
+            canceled_at: response.data.canceled_at,
+          }
+          : appointment
+        )
+      )
+  }
+
   return (
     <Background >
       <Container>
@@ -29,7 +41,7 @@ export default function Dashboard() {
           data={appointments}
           keyExtractor = {item => String(item.id)}
           renderItem={({ item }) => (
-            <Appointment data={item} />
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
           )}
         />
 
