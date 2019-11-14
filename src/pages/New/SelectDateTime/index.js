8d83e4073a3fd05dc6ef0d0078342a5abd7c7706
Delfio <delfio_eu@hotmail.com>
2019-11-14 14:13:00 -0400

    StackNavigator - agendar horarios

---
+++ src/pages/New/SelectDateTime/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '../../../components/Background';
+import DateInput from '../../../components/DateTimeInput';
+
+import { Container } from './styles';
+
+export default function SelectDateTime() {
+  return (
+    <Background>
+      <Container>
+        <DateInput />
+      </Container>
+    </Background>
+  );
+}
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o melhor Horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
---
+++ src/pages/New/SelectProvider/index.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../../services/api';
+
+import Background from '../../../components/Background';
+
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url.replace('localhost', '192.168.1.4')
+                    : `https://api.adorable.io/avatar/50/kkkkkk.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
+}
+
+SelectProvider.navigationOptions = ({ navigation }) => ({
+  title: 'selecione o prestador',
+  titleStyle: {
+    alignSelf: 'center',
+  },
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Dashboard');
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
