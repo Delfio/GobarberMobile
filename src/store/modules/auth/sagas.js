cff3af8785f12ccc473e08f6be59fee6f214626b
Delfio <delfio_eu@hotmail.com>
2019-11-14 00:39:20 -0400

    Logout

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,9 +1,9 @@
 import { Alert } from 'react-native';
-import {  takeLatest, call, put, all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import api from '../../../services/api';
 
-import { signInSucess, signFailure } from './actions'
+import { signInSucess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
   try {
@@ -11,13 +11,16 @@ export function* signIn({ payload }) {
 
     const response = yield call(api.post, 'sessions', {
       email,
-      password
+      password,
     });
 
     const { token, user } = response.data;
 
-    if(user.provider){
-      Alert.alert('Erro no login', 'O usúario não pode ser prestador de serviços' )
+    if (user.provider) {
+      Alert.alert(
+        'Erro no login',
+        'O usúario não pode ser prestador de serviços'
+      );
       return;
     }
 
@@ -26,15 +29,15 @@ export function* signIn({ payload }) {
 
     yield put(signInSucess(token, user));
 
-   // history.push('/dashboard');
-  } catch(err){
-    Alert.alert('Falha na autenticação', 'Erro no login' )
+    // history.push('/dashboard');
+  } catch (err) {
+    Alert.alert('Falha na autenticação', 'Erro no login');
     yield put(signFailure());
   }
-};
+}
 
-export function* signUp ({ payload }) {
-  try{
+export function* signUp({ payload }) {
+  try {
     const { name, email, password } = payload;
 
     yield call(api.post, 'users', {
@@ -43,33 +46,28 @@ export function* signUp ({ payload }) {
       password,
     });
     // history.push('/')
-
-  }catch(err){
-    Alert.alert('Erro no cadastro', 'Houve um erro! favor verifique seus dados' )
+  } catch (err) {
+    Alert.alert(
+      'Erro no cadastro',
+      'Houve um erro! favor verifique seus dados'
+    );
     yield put(signFailure());
   }
 }
-export function setToken ({ payload }) {
+export function setToken({ payload }) {
   const { token } = payload.auth;
 
-  if(!payload) {
-    console.log("asdfasdf")
-    return
+  if (!payload) {
+    return;
   }
 
-  if(token) {
+  if (token) {
     api.defaults.headers['Authorization'] = `Bearer ${token}`;
   }
-
-}
-
-export function signOut(){
-  // history.push('/');
 }
 
 export default all([
   takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
-  takeLatest('@auth/SIGN_OUT', signOut),
 ]);
