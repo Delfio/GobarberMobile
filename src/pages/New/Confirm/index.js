fb97bcf9880b8ae1ee378c1ee53804b46aea667b
Delfio <delfio_eu@hotmail.com>
2019-11-14 18:31:19 -0400

    Confirmar Agendamento

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,55 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import Background from '../../../components/Background';
 
-export default function Confirm() {
-  return <View />;
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirm({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const timeFormated = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  );
+
+  async function handleAddAppointment() {}
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url.replace('localhost', '192.168.1.4')
+              : `https://api.adorable.io/avatar/50/kkkkkk.png`,
+          }}
+        />
+        <Name>{provider.name}</Name>
+
+        <Time>{timeFormated}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar Agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirme seu Agendamento',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
